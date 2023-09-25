af6b19ac31f9c9eab7d8f08027881100b9321f89
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-09-25 19:02:14 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,40 @@
+output "id" {
+  value = [
+    for cluster in azurerm_kubernetes_cluster.azrk8sclst: cluster.id
+  ]
+}
 
+output "kube_config" {
+  sensitive = true
+  value = [ 
+    for cluster in azurerm_kubernetes_cluster.azrk8sclst: cluster.kube_config_raw
+  ]
+}
+
+output "client_key" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.azrk8sclst: cluster.kube_config.0.client_key
+  ]
+}
+
+output "client_certificate" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.azrk8sclst: cluster.kube_config.0.client_certificate
+  ]
+}
+
+output "cluster_ca_certificate" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.azrk8sclst: cluster.kube_config.0.cluster_ca_certificate
+  ]
+}
+
+output "host" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.azrk8sclst: cluster.kube_config.0.host
+  ]
+}
